f67079da64a59bb3fb686a100680430b12e9ed1e
michael.roosz <michael.roosz@check24.de>
2023-12-08 15:01:46 +0100

    Simplify example2

--- pages/index/+Page.vue
+++ pages/index/+Page.vue
@@ -1,29 +1,13 @@
 <template>
-  <component :is="getComponent('Test1')" />
+  <div>
+    <component :is="getComponent('Test1')" />
+  </div>
 </template>
 
 
-<script lang="ts" setup>
+<script setup lang="ts">
 import { defineAsyncComponent } from 'vue';
 
-const rawComponents = import.meta.glob('/renderer/**/*.vue');
-const components = new Map<string, () => Promise<{ [key: string]: unknown; }>>();
-
-for (const path in rawComponents) {
-  const name = path.split('/').reverse()[0].replace('.vue', '');
-    components.set(name, rawComponents[path]);
-}
-
-const getComponent = (name : string) => {
-  const ci =components.get(name);
-
-  if (ci !== undefined) {
-      return defineAsyncComponent(async () => {
-          return ci();
-      });
-  }
-
-  return undefined;
-};
+const getComponent = (name : string) =>  defineAsyncComponent(() => import(`../../renderer/${name}.vue`));
 
 </script>
